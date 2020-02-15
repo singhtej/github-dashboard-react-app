7438660c9447009aa06b91ced70dc914fad27101
Vignesh Narendran <vignesh-narendran@users.noreply.github.com>
2020-02-15 21:34:15 +0530

    Updates

---
+++ precache-manifest.43c1d483e6e8342d6abdc9bd27db2e6a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b82a7948320a2a8414774d7fec97913d",
+    "url": "/my-github-dashboard-react-app/index.html"
+  },
+  {
+    "revision": "70bd4a1b1fe48592700e",
+    "url": "/my-github-dashboard-react-app/static/css/main.e99da6e0.chunk.css"
+  },
+  {
+    "revision": "5639f19878d0d9b6d8d5",
+    "url": "/my-github-dashboard-react-app/static/js/2.637bce1a.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/my-github-dashboard-react-app/static/js/2.637bce1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70bd4a1b1fe48592700e",
+    "url": "/my-github-dashboard-react-app/static/js/main.033789a1.chunk.js"
+  },
+  {
+    "revision": "02777a5f91b7586fdaf6",
+    "url": "/my-github-dashboard-react-app/static/js/runtime-main.03dea1a4.js"
+  },
+  {
+    "revision": "5de5d82ae691399138f3a3d7873dbb4b",
+    "url": "/my-github-dashboard-react-app/static/media/Rolling-1.5s-264px.5de5d82a.gif"
+  }
+]);
